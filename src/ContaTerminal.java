3fa2ad0e9053b0bdb7634fa5d5f93a7043b5355e
Mac-Swell Santos <macswell.salviano@gmail.com>
2025-05-07 19:16:12 -0300

    Commit inicial

---
+++ src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.";
+        System.out.println(mensagem);
+        scanner.close();
+
+    }
+}
